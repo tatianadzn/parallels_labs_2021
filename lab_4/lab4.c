c315ea8b532f6a22bbce3d539d5e0f9e2a4b9d0b
Yunir Salimzyanov <yunir.me@gmail.com>
2021-11-14 20:05:15 +0300

    Implement lab 4 task1 part

---
+++ lab_4/lab4.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <math.h>
+
+#ifdef _OPENMP
+#include "omp.h"
+#else
+void omp_set_num_threads(int M) { }
+#endif
+
+void generate_array(double *array, int size, unsigned int *seed, int min, int max) {
+    //printf("--- generate:\n");
+    #pragma omp parallel for default(none) shared(array, size, seed, min, max)
+    for (int j = 0; j < size; j++) {
+        unsigned int seed_j = j + *seed;
+        double random = ((double) rand_r(&seed_j)) / (double) RAND_MAX;
+        double diff = max - min;
+        double r = random * diff;
+        array[j] = min + r;
+        //printf("j=%d array[j]=%f\n", j, array[j]);
+    }
+}
+
+void array_copy(const double *original, double *copied, int size) {
+    copied[0] = 0;
+    //printf("--- map: copy\n");
+    #pragma omp parallel for default(none) shared(size, original, copied)
+    for (int i = 0; i < size; i++) {
+        copied[i + 1] = original[i];
+        //printf("i=%d copied[i+1]=%f=original[i]=%f\n", i, copied[i+1], original[i]);
+    }
+    //printf("...\n");
+}
+
+/* Combsort: function to find the new gap between the elements */
+int newgap(int gap) {
+    gap = (gap * 10) / 13;
+    if (gap == 9 || gap == 10)
+        gap = 11;
+    if (gap < 1)
+        gap = 1;
+    return gap;
+}
+
+/* Combsort: implementation */
+void combsort(double a[], int aSize) {
+    int gap = aSize;
+    double temp;
+    int i;
+    for (;;) {
+        gap = newgap(gap);
+        int swapped = 0;
+        for (i = 0; i < aSize - gap; i++) {
+            int j = i + gap;
+            if (a[i] > a[j]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                swapped = 1;
+            }
+        }
+        if (gap == 1 && !swapped)
+            break;
+    }
+}
+
+double find_min_in_sorted_arr(double *sorted_arr, int size) {
+    for (int i = 0; i < size; i++) {
+        if (sorted_arr[i] != 0) {
+            return sorted_arr[i];
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    const int A = 8 * 7 * 10; // Дзензура Татьяна Михайловна
+
+    unsigned int i, N, num_threads, seed;
+    struct timeval T1, T2;
+    long delta_ms;
+
+    if (argc != 3) {
+        printf("Usage: ./lab1 N num_threads\n");
+        printf("N - size of the array; should be greater than 2\n");
+        printf("num_threads - number of threads\n");
+        return 1;
+    }
+    N = atoi(argv[1]);
+    num_threads = atoi(argv[2]);
+
+    omp_set_num_threads(num_threads);
+
+    double *arr1 = malloc(sizeof(double) * N);
+    double *arr2 = malloc(sizeof(double) * (N / 2));
+    double *arr2_copy = malloc(sizeof(double) * (N / 2 + 1));
+
+    gettimeofday(&T1, NULL); /* запомнить текущее время T1 */
+
+    /* 100 экспериментов */
+    for (i = 0; i < 1; i++) {
+        seed = i;
+
+        /* Этап 1. Generate */
+        generate_array(arr1, N, &seed, 1, A);
+        generate_array(arr2, N / 2, &seed, 1, A);
+
+        /* Этап 2. Map */
+        // M1 (e -> hyperbolic cos(e) + 1)
+        //printf("--- map: hyperbolic cos(e) + 1\n");
+        #pragma omp parallel for default(none) shared(N, arr1)
+        for (int j = 0; j < N; j++) {
+            arr1[j] = cosh(arr1[j]) + 1;
+            //printf("j=%d arr1[j]=%f\n", j, arr1[j]);
+        }
+        //printf("...\n");
+
+        // M2 -> M2_copy   {0, M2[0], M2[1]...}
+        // e.g:
+        // M2       : 1 2 3 4 5 6
+        // M2_copy  : 0 1 2 3 4 5
+
+        array_copy(arr2, arr2_copy, N / 2);
+
+        // M2 (e -> abs(ctg( (e-1)+e )) )
+        //printf("--- map: abs(ctg( (e-1)+e )\n");
+        #pragma omp parallel for default(none) shared(N, arr2, arr2_copy)
+        for (int j = 0; j < N / 2; j++) {
+            arr2[j] = fabs(1 / tan(arr2[j] + arr2_copy[j]));
+            //printf("j=%d arr2[j]=%f\n", j, arr2[j]);
+        }
+        //printf("...\n");
+
+        /* Этап 3. Merge */
+        // M1, M2 (e1, e2 -> e1 / e2)
+        //printf("--- merge: e1 / e2\n");
+        #pragma omp parallel for default(none) shared(N, arr1, arr2)
+        for (int j = 0; j < N / 2; j++) {
+            arr2[j] = arr1[j] / arr2[j];
+            //printf("j=%d arr2[j]=%f\n", j, arr2[j]);
+        }
+        //printf("...\n");
+
+        /* Этап 4. Sort */
+        combsort(arr2, N / 2);
+
+        /* Этап 5. Reduce */
+        double X = 0;
+        double min = find_min_in_sorted_arr(arr2, N / 2);
+        if (min == 0) exit(0);
+        //printf("--- reduce: + sin\n");
+        #pragma omp parallel for default(none) shared(N, arr2, min) reduction(+:X)
+        for (int j = 0; j < N / 2; j++) {
+            if ((int) (arr2[j] / min) % 2 == 0) {
+                X += sin(arr2[j]);
+            }
+        }
+        //printf("X=%f\n", X);
+        //printf("...\n");
+    }
+
+    gettimeofday(&T2, NULL); /* запомнить текущее время T2 */
+
+    free(arr1);
+    free(arr2);
+    free(arr2_copy);
+
+    delta_ms = 1000 * (T2.tv_sec - T1.tv_sec) + (T2.tv_usec - T1.tv_usec) / 1000;
+
+    printf("%d,%ld\n", N, delta_ms);
+//    printf("N=%d. Milliseconds passed: %ld\n", N, delta_ms);
+    return 0;
+}
---
+++ lab_4/scripts/compile.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+FILENAME=lab4
+
+WORK_DIR=".."
+OUT_DIR="$WORK_DIR/bin"
+mkdir -p $OUT_DIR
+
+# Compile
+gcc -O3 -Wall -Werror -Wno-unknown-pragmas -o $OUT_DIR/${FILENAME}-wo-openmp $WORK_DIR/${FILENAME}.c -lm
+gcc -O3 -Wall -Werror -fopenmp -o $OUT_DIR/${FILENAME}-openmp $WORK_DIR/${FILENAME}.c -lm
